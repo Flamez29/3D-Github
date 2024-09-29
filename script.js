b200695bf97d9afd94518f51cb014c6bed517fc2
Flamez001 <flameztech001@gmail.com>
2024-09-29 06:07:51 +0100

    ADDED A CLICK EVENT FOR AUDIO

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ window.onload = function() {
       document.getElementById("loader").remove();
     }, 2000); // Remove loader after 2 seconds (adjust as needed)
   }
+
+  const audio = document.getElementById('myAudio');
+  let tapCount = 0;
+  
+  document.addEventListener('click', () => {
+    tapCount++;
+  
+    if (tapCount === 2) {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+      tapCount = 0; // Reset the tap count
+    }
+  });
